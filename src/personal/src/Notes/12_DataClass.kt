ad763fec8e540f1abf3cdc09b9f7dc1c88a86993
Kuday Ogunlu <k.ogunlu@gmail.com>
2022-07-02 17:05:13 +0300

    12_DataClass.kt and 13_EnumClass.kt are added.

---
+++ src/personal/src/Notes/12_DataClass.kt
@@ -0,0 +1,159 @@
+package personal.src.Notes
+
+/**
+ *
+ *      DATA CLASS
+ *
+ */
+
+//----------------------------------------------------------------------------------------------------------------------
+
+/*
+DATA CLASS NEDIR?
+-- Veri / value tuttugumuz classtir (örnegin DB den gelen veriler)
+-- Loglamasi daha kolay oluyor, classta neler oldugu gorebiliyoruz
+-- toString, hashCode, copy, equal gibi fonksiyonlari kolayca kullanabiliyoruz override eetmeden.
+-- her component icin component1, component2 gibi (parametre sayisi kadar) compononentN kadar yazilir otomatik olarak. Bu sekilde kodun okunakligi artar.
+-- Super type olarak kullanilamaz. Miras alinamaz, ust sinif olamaz.
+-- Bir interface'i implement edebilir ya da miras alabilir.
+-- Icindeki, constructor disinda yazilan degiskenler bu otomatik fonksiyonlari almaz. Elle yazmamiz gerekir.
+-!!- Var / val olarak yazilmasi zorunludur.
+    CUNKU; parametrelere class disinda da erismek istiyoruz, erisebilmek icin bu sekilde yaratiyoruz.
+    equal, hashCode gibi kodlari kullanabilmek icin bunlara erisebiliyor olmamiz gerekir. Bu fonksiyonlar arkada yazilirken bu degiskenlere erismek isteyecek.
+
+DATA Class vs Duz Class farki nedir?
+--Data classta bazi fonksiyonlar otomatik yazilirken, duz class'ta asagida verlien fonksiyonlar otomatik olarak olusturulmuyor
+    (hashCode, copy, toString, equalst etc.)
+
+
+1- Data class'lar en azindan mutlaka 1 parametre almak zorundadir
+2- Parametreler mutlaka val ya da var ile tanimlanmak zorundadir
+3- Open, abstract, sealed, inner class yapilamaz
+4- Tüm data class'lari final oldugu icin final modifer'i yazmak redundant uyarisi verecektir
+
+equals, hashCode, toString, copy, componentN fonksiyonlari arka planda default olarak olusturulur
+
+Yukaridaki fonksiyonlardan biri override edilirse, oto generate edilen hali yazilmaz
+
+Yukaridaki fonksiyonlar sadece promary const. icerisindeki degiskenlerle calisir
+
+componentN fonksiyonlari sebebiyle destructturing declaration olarak data class'lar set edilebiliyor.
+    --
+
+toString gibi fonksiyonlarda primary const. degiskenleri kullanildigi icin val ya da var yazilmak zorunludur
+
+Pair, Triple ozellestirilmis generic data class'lar mevcut
+
+ */
+
+//----------------------------------------------------------------------------------------------------------------------
+
+data class News(                             // open yapamiyoruz, kendinden final'dir yani miras alinamaz, fakat miras alabilir
+    val title: String = "Title",
+    val description: String,
+    val hasMediaContent: Boolean,               // bu degiskenlerin equals, hashCode gibi yukarda yazan fonksiyonlari default olarak otomatik olusturuluyor
+    val mediaList: List<Media>
+) {
+    val relatedNewsList: List<News> =
+        arrayListOf()       // contstructor disinda, iceriye uye bir degisken yazarsak, bu otomatik fonksiyonlar default olusturulmayacak
+
+    override fun toString(): String {
+        return "return super.toString()"
+    }
+
+    fun log() {
+        println("Title: $title, description: $description")
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+class NewsData(
+    val title: String = "Title",
+    val description: String,
+    val hasMediaContent: Boolean,
+    val mediaList: List<Media>
+) {
+    val relatedNewsList: List<News> = arrayListOf()
+
+    override fun toString(): String {
+        return "News(title= " + this.title + ", description=" + this.title + "hasMediaContent=" + this.hasMediaContent + "mediaList" + this.mediaList
+    }
+}
+
+class Media
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+fun main() {
+    val newsOne = News(
+        title = "Yeni bir egitim donemi basladi!",
+        description = "Egitim donemi yeni bir sekilde basladi",
+        hasMediaContent = true,
+        mediaList = arrayListOf()
+    )
+
+    val newsTwo = NewsData(
+        "Yeni bir egitim donemi basladi!",
+        "Egitim donemi yeni bir sekilde basladi",
+        true,
+        arrayListOf()
+    )
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    // okunakliligi arttirmak icin destructturing declaration yapabiliyoruz data class'larda
+    // arka planda component olarak ataniyor otomatik, bu nedenle dogrudan cagirim yapabiliyoruz
+    // component1, component2, ...
+
+    val (title, description, hasMediaContent, mediaList) = newsOne
+    val (title3, description3) = newsOne
+    val (title4, description4, hasMediaContent4, mediaList4) = newsOne
+
+ //   val (title2, description2, hasMediaContent2, mediaList2) = newsTwo      // duz class oldugu icin yapamiyoruz
+
+    newsOne.mediaList // boyle ulasmak yerine
+    mediaList  // seklinde de ulasabiliriz
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    newsOne.toString()
+    newsOne.hashCode()
+
+    val newsThree =
+        newsOne.copy(description = "Farkli description")  // 0'dan yaratmak yerine bu nesne ile ayni olsun, sadece description'i farkli olsun dedik
+    newsOne.component1()
+
+    val newsFour =
+        News(                // data class degil, data class olsa ve copy kullanamasak boyle yazmamiz gerekecekti.
+            title = newsOne.title,
+            description = "Farkli description",
+            hasMediaContent = newsOne.hasMediaContent,
+            mediaList = newsOne.mediaList
+        )
+
+    newsOne.relatedNewsList
+    newsOne.title
+
+    // newsTwo.equals()
+    // newsTwo.copy()
+    //newsTwo.component1()
+    newsTwo.toString()
+    newsTwo.hashCode()
+
+    println("Data class toString: $newsOne") //constructor parametreleri verecek dataClass oldugu icin
+    println("Class to String: $newsTwo")  // icindeki parametreleri vermeyecek
+
+
+
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+
+
---
+++ src/personal/src/Notes/13_EnumClass.kt
@@ -0,0 +1,269 @@
+package personal.src.Notes
+
+import java.util.*
+
+/**
+ *
+ *      ENUM CLASS
+ *
+ */
+
+//----------------------------------------------------------------------------------------------------------------------
+
+/*
+
+Tip guvenligini (type-safety) saglar
+Kullanim amaci ayni veri kumelerinin gruplanabilir olmasini saglar
+Opsiyonlarin tamamaminin neler oldugunu rahatca gorebilmemizi saglar
+
+abstract, openi inner, sealed olamazlar
+yapisi geregi zaten final'dir. Final keyword'u kullanmak redundant uyarisi verir
+
+ozunde class olduklari icin, constructor kullanabilirler ve bu constructor icersiinde veri tutabilirler
+java karsiliginda bu enum sabitleri static final class'lar seklinde tutulurlar
+    bu sayede, kullanirken bu sabitlerin nesnesini olusturmak zorunda kalmayiz.
+
+enum class'larin nesnesi olusuturulmaz.
+
+Her enum sabiti final name: String ve final ordinal: Int degiskenler default olarak sahiptir
+name ifadesi, enum'in kendisinin string halini verir
+ordinal ifadesi, enum'daki sabitin index'ini verir ve 0'dan baslar.
+bu degiskenler final olduklari icin, enum sabitleri icerisinde override edilemezler
+
+enum class'lar herhangi baska bir class'i miras alamazlar
+enum class'lar herhangi baska bir interface'i implement edebilirler.
+
+enum class'lar abstract  property'ler ya da abstract func'lar alabilir
+Bunlari aldiklarinda tum sabitler bu abstract yapilari override etmek zorundadir.
+
+bir open function da yazilabilir. open olmasindan dolayi override etme zorunlulugu yoktur.
+Ancak open olarak belirtilen func'nun body'si olmak zorundadir.
+
+kullanim sekli;
+
+enum class xxxType{
+NAME1,
+NAME2,
+NAME3
+}
+
+
+Onemli not:
+Tum enum sabitleri aslinda bir alt class'tir, ust sinifi yani enum class'i miras alir (class ya da interface).
+Bu nedenle bu alt class'lari soyle cagirabiliriz
+Teams.BESIKTAS
+----Teams().BESIKTAS seklinde degil, nesne olusturmadan cagrilir
+
+
+ */
+
+enum class ColorType {
+    RED, BLUE, WHITE, GREEN
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+interface TeamsFunctionality {
+    fun practice()
+}
+
+
+enum class Teams(val starCount: Int) : TeamsFunctionality {
+
+    FENERBAHCE(3) {
+        override fun practice() {
+            TODO("Not yet implemented")
+        }
+    },
+
+    GALATASARAY(4) {
+        override fun practice() {
+            TODO("Not yet implemented")
+        }
+    },
+
+    BESIKTAS(3) {
+        override fun practice() {
+            TODO("Not yet implemented")
+        }
+    },
+
+    TRABZONSPOR(2) {
+        override fun practice() {
+            TODO("Not yet implemented")
+        }
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+enum class DaysOfWeek(val dayNumber: Int) {
+
+    PAZARTESI(1) {
+        override fun toString(): String {
+            return "PAZARTESİ"                  // normalde gerek olmayan bir sey, cunku zaten DaysOfWeek.PAZARTESI.name yazınca degeri verecek
+        }                                       // ekstra bir şey (türkçe karakter gibi) yapmak istersek override edelim, keko olmayalım
+    },
+
+    SALI(2),
+
+    CARSAMBA(3),
+
+    PERSEMBE(4),
+
+    CUMA(5),
+
+    CUMARTESI(6),
+
+    PAZAR(7)
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+enum class Sex{
+    Male {
+        override val typeCount: Int
+            get() = TODO("Not yet implemented")
+
+        override fun isOptional() {
+            TODO("Not yet implemented")
+        }
+    },
+
+    Female {
+        override val typeCount: Int
+            get() = TODO("Not yet implemented")
+
+        override fun isOptional() {
+            TODO("Not yet implemented")
+        }
+    },
+     Other{
+         override val typeCount: Int
+             get() = TODO("Not yet implemented")
+
+         override fun isOptional() {
+             TODO("Not yet implemented")
+         }
+
+     };
+
+// bir enum class'ina, her bir unem sabitinde override edilmesini istedigimiz seyler varsa
+    // mecburen interface implement etmek zorunda degiliz
+    // asagidaki gibi abstract val veya fun olusturabiliriz
+    // bu enum sabitleri aslinda bir CLASS oldugu icin, ust sinifin (Sex) abstract'larini override etmek zorunda kaliyor
+
+    abstract val typeCount : Int
+    abstract fun isOptional()
+    open fun log() {}       // open, override zorunlulugu yok
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+fun main() {
+
+    val fb = "FENERBAHCE"
+    val gs = "GALATASARAY"
+    val bjk = "BESIKTAS"
+    val ts = "TRABZONSPOR"
+
+    val pzt = "PAZARTESI"
+    val sal = "SALI"
+    val car = "CARSAMBA"
+    val per= "PERSEMBE"
+    val cum = "CUMA"
+    val cmt = "CUMARTESI"
+    val paz = "PAZAR"
+
+    val male = "MALE"
+    val female = "FEMALE"
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    println("4 buyuk takimdan birini yaziniz: ")
+    val team: String = readLine()!!
+
+    val starCount = when {
+        team == fb ->{
+            3
+        }
+        team == gs ->{
+            4
+        }
+        team == bjk -> {
+            3
+        }
+        team == ts -> {
+            2
+        }else -> {
+            -1
+        }
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    val starCount2 = when (team){
+
+      //  Alttaki kodun daha temiz yazilmis hali
+      //
+      //  Teams.FENERBAHCE.name.uppercase() -> {
+      //
+      //  }
+
+
+        Teams.FENERBAHCE.toString() -> {
+            Teams.FENERBAHCE.starCount
+        }
+        Teams.GALATASARAY.toString() -> {
+            Teams.GALATASARAY.starCount
+        }
+        Teams.BESIKTAS.toString() -> {
+            Teams.BESIKTAS.starCount
+        }
+        Teams.TRABZONSPOR.toString() -> {
+            Teams.TRABZONSPOR.starCount
+        }
+        else -> {
+            -1
+        }
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+println(DaysOfWeek.CARSAMBA.name)
+println(DaysOfWeek.CARSAMBA.toString())
+
+println(Teams.BESIKTAS.name)
+println(Teams.BESIKTAS.toString())
+println(Teams.BESIKTAS.starCount)
+
+println(DaysOfWeek.CARSAMBA.ordinal)
+println(DaysOfWeek.CARSAMBA.dayNumber)
+
+    // FENERBAHCE degeri enum olarak yoksa, error firlatir.
+    Teams.valueOf("FENERBAHCE")
+    // Teams icerisindeki tum enum constant'larini bir liste halinde doner.
+    Teams.values()
+
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+    val bestTeam = getBestTeam(Teams.BESIKTAS)
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+fun getBestTeam(team: Teams): Teams {
+    return team
+}
